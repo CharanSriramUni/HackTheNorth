b6894b6da7bed163880afb3af0471d540e678e63
Charan Sriram <91582356+CharanSriramUni@users.noreply.github.com>
2023-09-16 23:41:09 -0400

    final changes

---
+++ backend/test/test.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws');
+const fs = require('fs');
+
+const ws = new WebSocket('ws://localhost:3001');
+
+ws.on('open', () => {
+    console.log('Connected to the WebSocket server.');
+});
+
+ws.on('message', (data) => {
+    fs.writeFileSync('./test.html', data);
+});
+
+ws.on('close', () => {
+    console.log('Disconnected from the WebSocket server.');
+});
+
+ws.on('error', (error) => {
+    console.error('WebSocket Error:', error);
+});
