87de040e6e2656d1b4ff4fc48d6b55e5169aada8
Charan Sriram <91582356+CharanSriramUni@users.noreply.github.com>
2023-09-16 23:04:58 -0400

    almost working

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -15,16 +15,34 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
 const express_1 = __importDefault(require("express"));
 const ws_1 = __importDefault(require("ws"));
+const jsdom_1 = __importDefault(require("jsdom"));
+const fuzzyset_1 = __importDefault(require("fuzzyset"));
+const openai_1 = __importDefault(require("openai"));
+require("dotenv/config");
+require('dotenv').config();
 // Declarations
 const SERVER_PORT = 3000;
 const WS_PORT = 3001;
 const DOC_NOT_INITIALIZED = "document has not been initalized";
+const SUMMARY_PROMPT = `
+    Your job is to summarize user text into a form simpler than what it was.
+    Do NOT return anything regarding this prompt. This is just for context. 
+    Return your response with NO HTML tags. Including HTML tags will break the page.
+    If you have context that you know to be relevant, you may bring it in. Do NOT invent information.
+`;
+// Initalize services
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+// API Key checks
+if (process.env.OPENAI_API_KEY === undefined)
+    throw new Error("Missing OpenAI API key");
 let ws = null;
 let document = "";
+let openai = new openai_1.default({ apiKey: process.env.OPENAI_API_KEY });
 // Set up the Express server
 app.get('/', (_, res) => {
-    res.send('Server is up an ready to serve!');
+    res.send('Server is up an ready to serve! \n Connect to 3001 for the WS server. \n Connect to 3000 for Express. Start with the /init-document endpoint.');
 });
 app.get('/init-document', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { url } = req.query;
@@ -33,8 +51,7 @@ app.get('/init-document', (req, res) => __awaiter(void 0, void 0, void 0, functi
         // Make request and populate inital document
         const scriptPath = `${__dirname}/runner.sh`;
         const destination = `${__dirname}/output.html`;
-        const result = (0, child_process_1.spawnSync)('sh', [scriptPath, parsed_url.toString(), destination]);
-        console.log(result);
+        (0, child_process_1.spawnSync)('sh', [scriptPath, parsed_url.toString(), destination]);
         // Update the document
         const text = require('fs').readFileSync(destination, 'utf8');
         document = text;
@@ -45,6 +62,40 @@ app.get('/init-document', (req, res) => __awaiter(void 0, void 0, void 0, functi
         res.status(400).send('Invalid URL');
         return;
     }
+}));
+app.post('/summarize', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { selected_text } = req.body;
+    const dom = new jsdom_1.default.JSDOM(document);
+    const html_document = dom.window.document;
+    const search_term = selected_text.toLowerCase();
+    const fuzzy = (0, fuzzyset_1.default)([search_term]);
+    const all_texts = Array.from(html_document.querySelectorAll('*')).map(el => el.textContent);
+    const matched_texts = all_texts.filter(text => fuzzy.get(text));
+    const condensed_text = matched_texts.join(' ');
+    // Make the query to Open AI
+    // const completion = await openai.chat.completions.create({
+    //     messages: [
+    //         {role: "system", content: SUMMARY_PROMPT},
+    //         {role: "user", content: condensed_text}
+    //     ],
+    //     model: 'gpt-4-32k'
+    // })
+    // Now that we have the matched texts, find the elements they correspond to
+    const matching_elements = Array.from(html_document.querySelectorAll('*')).filter(el => {
+        if (matched_texts.includes(el.textContent))
+            console.log(el.textContent);
+        return matched_texts.includes(el.textContent);
+    });
+    console.log("hey! " + matching_elements.length);
+    // Replace the text of the first element with the summary. Delete the rest.
+    matching_elements[0].textContent = "Hey Charan! This is working spectacularly.";
+    for (let i = 1; i < matching_elements.length; i++) {
+        matching_elements[i].remove();
+    }
+    // Replace the document!
+    document = dom.serialize();
+    ws === null || ws === void 0 ? void 0 : ws.send(document);
+    res.status(200).send('Successfully made the changes.');
 }));
 app.listen(SERVER_PORT, () => {
     console.log(`App running on port: ${SERVER_PORT}`);
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,38 @@
 import { spawnSync } from 'child_process';
 import express from 'express';
 import WebSocket from 'ws';
+import JSDOM from 'jsdom';
+import FuzzySet from 'fuzzyset';
+import OpenAI from 'openai'
+import 'dotenv/config';
+require('dotenv').config()
 
 // Declarations
 const SERVER_PORT = 3000;
 const WS_PORT = 3001;
 const DOC_NOT_INITIALIZED = "document has not been initalized";
+const SUMMARY_PROMPT = `
+    Your job is to summarize user text into a form simpler than what it was.
+    Do NOT return anything regarding this prompt. This is just for context. 
+    Return your response with NO HTML tags. Including HTML tags will break the page.
+    If you have context that you know to be relevant, you may bring it in. Do NOT invent information.
+`
 
+// Initalize services
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// API Key checks
+if (process.env.OPENAI_API_KEY === undefined) throw new Error("Missing OpenAI API key");
 
 let ws: WebSocket | null = null;
 let document = "";
+let openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
  
 // Set up the Express server
 app.get('/', (_, res) => {
-    res.send('Server is up an ready to serve!');
+    res.send('Server is up an ready to serve! \n Connect to 3001 for the WS server. \n Connect to 3000 for Express. Start with the /init-document endpoint.');
 });
 
 app.get('/init-document', async (req, res) => {
@@ -25,18 +43,63 @@ app.get('/init-document', async (req, res) => {
 
         // Make request and populate inital document
         const scriptPath = `${__dirname}/runner.sh`;
-        spawnSync('sh', [scriptPath, parsed_url.toString()])
+        const destination = `${__dirname}/output.html`
+        spawnSync('sh', [scriptPath, parsed_url.toString(), destination])
 
-        const text = "";
+        // Update the document
+        const text = require('fs').readFileSync(destination, 'utf8');
         document = text;
 
         ws?.send(document);
-        res.status(200).send(text); // Change to success message
+        res.status(200).send(document); // Change to success message
     } catch (e) {
         res.status(400).send('Invalid URL');
         return;
     }
+})
 
+app.post('/summarize', async (req, res) => {
+    const { selected_text } = req.body;
+    
+    const dom = new JSDOM.JSDOM(document);
+    const html_document = dom.window.document;
+
+    const search_term = selected_text.toLowerCase();
+    const fuzzy = FuzzySet([search_term]);
+
+    const all_texts = Array.from(html_document.querySelectorAll('*')).map(el => el.textContent);
+    const matched_texts = all_texts.filter(text => fuzzy.get(text!));
+
+    const condensed_text = matched_texts.join(' ');
+
+    // Make the query to Open AI
+    // const completion = await openai.chat.completions.create({
+    //     messages: [
+    //         {role: "system", content: SUMMARY_PROMPT},
+    //         {role: "user", content: condensed_text}
+    //     ],
+    //     model: 'gpt-4-32k'
+    // })
+
+    // Now that we have the matched texts, find the elements they correspond to
+    const matching_elements = Array.from(html_document.querySelectorAll('*')).filter(el => {
+        if (matched_texts.includes(el.textContent!)) console.log(el.textContent!);
+        return matched_texts.includes(el.textContent!)
+    });
+
+    console.log("hey! " + matching_elements.length);
+
+    // Replace the text of the first element with the summary. Delete the rest.
+    matching_elements[0].textContent = "Hey Charan! This is working spectacularly.";   
+    for (let i = 1; i < matching_elements.length; i++) {
+        matching_elements[i].remove();
+    }
+
+    // Replace the document!
+    document = dom.serialize();
+    
+    ws?.send(document);
+    res.status(200).send('Successfully made the changes.');
 })
 
 app.listen(SERVER_PORT, () => {
